96f993b40b363223d7460c4252a1d72a9f4f3a5a
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2025-08-21 17:17:15 -0400

    install script defined

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add Docker's repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker packages
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install QEMU to allow Docker to emulate ARM instructions
+sudo apt-get update
+sudo apt-get install qemu-user-static binfmt-support
+
+# Tell Docker to use buildx to create a new "builder instance" to use for builds
+docker buildx create --use
+
+# Tell new "builder instance" to download necessary drivers/qemu binaries and list available platforms
+docker buildx inspect --bootstrap
+
